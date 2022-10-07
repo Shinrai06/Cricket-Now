7eea3028110112636ed87ecefb44a428a08432a4
Shinrai <98462700+Shinrai06@users.noreply.github.com>
2022-10-07 10:22:58 +0530

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -1,4 +1,4 @@
-const API_KEY = "532e9015-edf9-41a9-903d-961d96b2d0f3";
+const API_KEY = "";
 
 export const getMatches = ()=> {
     const url = `https://api.cricapi.com/v1/cricScore?apikey=${API_KEY}`;
